31805914dadb8947ac8783ded842feeaebe771b8
LightHillNess <104416322+LightHillNess@users.noreply.github.com>
2024-04-26 23:46:51 +0300

    Add project files.

---
+++ zad2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        // Входни данни - низ, в който ще търсим телефонни номера
+        string input = Console.ReadLine();
+
+        // Регулярен израз за телефонни номера от София
+        string regex = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+        // Търсене на съвпадения
+        MatchCollection matches = Regex.Matches(input, regex);
+
+        // Отпечатване на валидните телефонни номера
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+}
