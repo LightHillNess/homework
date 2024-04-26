9611c7ce7483d3227777e1787cd905725434e894
LightHillNess <104416322+LightHillNess@users.noreply.github.com>
2024-04-26 22:59:23 +0300

    Add project files.

---
+++ zad1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+ 
+        string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+ 
+        string input = "Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan Ivanov";
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+}
